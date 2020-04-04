e92c8e7898aa7bc5bbfefe0ab8857a035a1023b3
reedm26 <reedm26@yahoo.com>
2020-04-03 23:52:40 -0600

    4/3 daily challenges

--- DailyChallenges.js
+++ DailyChallenges.js
@@ -196,3 +196,25 @@ function Go(num) {
 function isFourLetters(arr) {
   return arr.filter(a => a.length == 4);
 }
+
+//This Triangular Number Sequence is generated from a pattern of dots that form a triangle. The first 5 numbers of the sequence, or dots, are: 1, 3, 6, 10, 15. Write a function that converts n number of places with its corresponding number.
+function triangle(n) {
+  return (n * (n += 1)) / 2;
+}
+
+//In semantic versioning a piece of software can be represented in a format like this example: 6.1.9.
+//The first number is the major version.
+//The second number is the minor version.
+//The third number is the patch (bug fixes).
+//Write three separate functions, one to retrieve each element in the semantic versioning specification.
+function retrieveMajor(semver) {
+  return semver.split(".")[0];
+}
+
+function retrieveMinor(semver) {
+  return semver.split(".")[1];
+}
+
+function retrievePatch(semver) {
+  return semver.split(".")[2];
+}
