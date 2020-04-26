fb97a4937e70b62e78b62d4941bf61ee63dac48c
reedm26 <reedm26@yahoo.com>
2020-04-25 23:31:41 -0600

    4/25

--- DailyChallenges2.js
+++ DailyChallenges2.js
@@ -11,3 +11,12 @@ function reverse(bool) {
 
 // Write a regular expression that matches a string if it contains at least one digit.
 let x = /\d/;
+
+// Christmas Eve is almost upon us, so naturally we need to prepare some milk and cookies for Santa! Create a function that accepts year, month and day as integers and returns true if it's Christmas Eve (December 24th) and false otherwise.
+// public class Program
+// {
+//     public static bool TimeForMilkAndCookies(int year, int month, int day)
+//     {
+// 			return (month == 12 && day == 24);
+//     }
+// }
