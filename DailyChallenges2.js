f874858ca0a5fddc2d770aa5a3778ae1b0c83096
reedm26 <reedm26@yahoo.com>
2020-04-27 23:42:18 -0600

    4/27

--- DailyChallenges2.js
+++ DailyChallenges2.js
@@ -25,3 +25,16 @@ let x = /\d/;
 function hasKey(obj, key) {
   return key in obj;
 }
+
+//Create a function that takes two arguments (item, times). The first argument (item) is the item that needs repeating while the second argument (times) is the number of times the item is to be repeated. Return the result in an array.
+function repeat(item, times) {
+  let newArr = [];
+  for (let i = 0; i <= times; i++) {
+    i = newArr.push(item);
+  }
+  if (times == 0) {
+    return [];
+  } else {
+    return newArr;
+  }
+}
