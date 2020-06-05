0ceb5ff9c187e1fab0aec76c9474ed07092f86c2
reedm26 <reedm26@yahoo.com>
2020-06-04 18:24:09 -0600

    6/4

--- Daily Challenges3.js
+++ Daily Challenges3.js
@@ -171,3 +171,8 @@ function reverseCapitalize(str) {
     .join("");
   return x.toString();
 }
+
+// Create a recursive function that takes two parameters and repeats the string n number of times. The first parameter txt is the string to be repeated and the second parameter is the number of times the string is to be repeated.
+function repetition(txt, n) {
+  return txt.repeat(n);
+}
