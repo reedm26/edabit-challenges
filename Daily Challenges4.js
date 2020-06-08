89a193b2eaec751311a49d0b7a19026e6ab93c45
reedm26 <reedm26@yahoo.com>
2020-06-07 22:50:09 -0600

    6/7

---
+++ Daily Challenges4.js
@@ -0,0 +1,4 @@
+// Create a function that takes an array and returns the difference between the biggest and smallest numbers.
+function diffMaxMin(arr) {
+  return Math.max(...arr) - Math.min(...arr);
+}
