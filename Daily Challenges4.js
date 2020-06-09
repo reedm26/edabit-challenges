ea8b33d752dd361291a73471dcb1bc33476ae70e
reedm26 <reedm26@yahoo.com>
2020-06-08 22:53:03 -0600

    6/8

--- Daily Challenges4.js
+++ Daily Challenges4.js
@@ -2,3 +2,12 @@
 function diffMaxMin(arr) {
   return Math.max(...arr) - Math.min(...arr);
 }
+
+// Write a function that checks if two numbers are: Smaller than 0, Greater than 0, Exactly 0
+function both(n1, n2) {
+  if (Math.sign(n1) === Math.sign(n2)) {
+    return true;
+  } else {
+    return false;
+  }
+}
