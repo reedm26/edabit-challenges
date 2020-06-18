141a4c3572d1f95f4940ae9bfced5b1c6456152c
reedm26 <reedm26@yahoo.com>
2020-06-17 22:43:19 -0600

    6/17

--- Daily Challenges4.js
+++ Daily Challenges4.js
@@ -50,3 +50,22 @@ function formatDate(date) {
 function checkPalindrome(str) {
   return [...str].reverse().join("") == str;
 }
+
+// If the number is a multiple of 3, return "Hello".
+// If the number is a multiple of 5, return "World".
+// If the number is a multiple of both 3 and 5, return "Hello World".
+function helloWorld(num) {
+  if (num % 5 == 0 && num % 3 == 0) {
+    return "Hello World";
+  } else if (num % 5 == 0) {
+    return "World";
+  } else if (num % 3 == 0) {
+    return "Hello";
+  } else {
+    return;
+  }
+}
+// OR
+function helloWorld(num) {
+  return num % 15 === 0 ? "Hello World" : num % 3 === 0 ? "Hello" : "World";
+}
