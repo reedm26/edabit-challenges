5e3c55b40361e5b2d113f17bf248fd08759a83a2
reedm26 <reedm26@yahoo.com>
2020-06-23 22:06:48 -0600

    6/23

--- DailyChallenges.cs
+++ DailyChallenges.cs
@@ -41,3 +41,12 @@ public class Program
   }
 
 }
+
+// Create a function that reverses a boolean value.
+public class Program
+{
+  public static bool Reverse(bool boolean)
+  {
+    return !boolean;
+  }
+}
