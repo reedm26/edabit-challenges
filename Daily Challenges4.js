3401b0956e77cea4a87c33318605c07030148233
reedm26 <reedm26@yahoo.com>
2020-06-25 19:37:03 -0600

    6/25

--- Daily Challenges4.js
+++ Daily Challenges4.js
@@ -99,3 +99,12 @@ const helloName = (name) => "Hello " + name + "!";
 function checkEnding(str1, str2) {
   return str1.endsWith(str2);
 }
+
+// Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+function findLargestNums(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(Math.max(...arr[i]));
+  }
+  return newArr;
+}
