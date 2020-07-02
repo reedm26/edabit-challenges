98116e0a90911ce42779b829486d4b046be6a919
reedm26 <reedm26@yahoo.com>
2020-07-01 19:01:16 -0600

    7/1

--- Daily Challenges4.js
+++ Daily Challenges4.js
@@ -145,3 +145,11 @@ const reverse = (txt) =>
     .reverse()
     .map((v) => (v === v.toLowerCase() ? v.toUpperCase() : v.toLowerCase()))
     .join("");
+
+// Write a function that takes all even-indexed characters and odd-indexed characters from a string and concatenates them together.
+function indexShuffle(str) {
+  const even = [...str].filter((char, i) => i % 2 === 0);
+  const odd = [...str].filter((char, i) => i % 2);
+
+  return [...even, ...odd].join("");
+}
