d6ef72cab9addba53b7cb6652a47dd7a4893b544
reedm26 <reedm26@yahoo.com>
2020-07-03 23:38:08 -0600

    7/3

--- Daily Challenges4.js
+++ Daily Challenges4.js
@@ -165,3 +165,12 @@ function toArray(num) {
 function toNumber(arr) {
   return Number(arr.join(""));
 }
+
+// Write a function that calculates the factorial of a number recursively.
+function factorial(n) {
+  if (n < 0) return -1;
+  else if (n == 0) return 1;
+  else {
+    return n * factorial(n - 1);
+  }
+}
