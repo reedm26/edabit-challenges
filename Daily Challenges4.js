58103042b0f2215e7ce3a1f4462757fa4fe6d4e4
reedm26 <reedm26@yahoo.com>
2020-07-09 23:15:05 -0600

    6/9

--- Daily Challenges4.js
+++ Daily Challenges4.js
@@ -208,3 +208,8 @@ function calcDeterminant(matrix) {
 function myPi(n) {
   return Number(Math.PI.toFixed(n));
 }
+
+// You are given an array with random words but your program doesn't accept words that begin with the capital letter "C". Remove the unaccepted words and return the new array.
+function acceptedWords(list) {
+  return list.filter((x) => /^[^C]/.test(x));
+}
